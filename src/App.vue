b790087bbb839d1408e25022981dcab45c7f040a
liufucheng <951562762@qq.com>
2021-05-26 16:38:48 +0800

    修改app

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div>11</div>
+    <div>112222222222222222222</div>
   </div>
 </template>
 
